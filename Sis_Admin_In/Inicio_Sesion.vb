0d3ab4379299d5253aef1e31ac3d11f94e056572
cardenas96 <jos.cardenas96@hotmail.com>
2019-08-11 21:08:35 -0500

    Lectura de txt, Proyecto de Administracion Basico

---
+++ Sis_Admin_In/Inicio_Sesion.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Data.SqlClient
+Public Class Inicio_Sesion
+    'Dim conexionsqls2 As New SqlConnection("Data Source ='.'; Initial Catalog = 'INVENTARIO_DB'; Integrated security = true")
+    Dim comando As SqlCommand = conexionsql.CreateCommand
+    Dim lector As SqlDataReader
+    Public bandera As Boolean = False
+    Private Sub Inicio_Sesion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub btn_aceptar_Click(sender As Object, e As EventArgs) Handles btn_aceptar.Click
+        If txt_usuario.Text = "" Or txt_cusuario.Text = "" Then
+            MessageBox.Show("Llene el campo vacio", "¡Error Campo Vacio!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            If txt_usuario.Text = "" Then
+                txt_usuario.Focus()
+            Else
+                txt_cusuario.Focus()
+            End If
+        Else
+            If bandera = False Then
+                conexionsql.Open()
+            Else
+                conexionsql.ConnectionString = "Data Source ='CARDENAS-PC'; Initial Catalog = 'INVENTARIO_DB'; Integrated security = true"
+                conexionsql.Open()
+                bandera = False
+            End If
+
+            Dim usuario, pass, u, c, t As String
+            usuario = txt_usuario.Text
+            pass = txt_cusuario.Text
+            Try
+                comando.CommandText = "SELECT nUsuario,cUsuario, tipo FROM USUARIOS WHERE nusuario = '" & usuario & "'"
+                lector = comando.ExecuteReader
+                lector.Read()
+                u = lector(0).ToString
+                c = lector(1).ToString
+                t = lector(2).ToString
+
+                lector.Close()
+                conexionsql.Close()
+                If usuario = u And pass = c Then
+                    Visible = False
+                    'SqlConnection.ClearAllPools()
+                    tipo = t
+                    Principal.ShowDialog()
+                    MsgBox("Termine")
+                    conexionsql.Dispose()
+                    conexionsql.Close()
+                Else
+                    MessageBox.Show("Usuario o Contraseña Incorrecto", "¡Error De Informacion!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    conexionsql.Close()
+                End If
+            Catch ex As Exception
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub btn_salir_Click(sender As Object, e As EventArgs) Handles btn_salir.Click
+        Dispose()
+    End Sub
+
+    Private Sub txt_cusuario_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_cusuario.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            btn_aceptar.PerformClick()
+        End If
+    End Sub
+
+    Private Sub txt_usuario_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_usuario.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            txt_cusuario.Focus()
+        End If
+    End Sub
+End Class
---
+++ Sis_Admin_In/conexionGlobal.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO 'esta libreria nos va a servir para poder activar el commandialog
+Imports Microsoft.Office.Interop
+Imports System.Data.SqlClient
+
+Module conexionGlobal
+    Public tipo As String
+    Public servidor As String = "."
+    Public conexionsql As New SqlConnection("Data Source ='" & servidor & "'; Initial Catalog = 'INVENTARIO_DB'; Integrated security = true")
+    'Dim conexionsql As New SqlConnection("Data Source ='.'; Initial Catalog = 'INVENTARIO_DB'; Integrated security = true ")
+    Dim comando As SqlCommand = conexionsql.CreateCommand
+
+    Function llenarExcel(ByVal ElGrid As DataGridView) As Boolean
+        'Creamos las variables
+        Dim exApp As New Microsoft.Office.Interop.Excel.Application
+        Dim exLibro As Microsoft.Office.Interop.Excel.Workbook
+        Dim exHoja As Microsoft.Office.Interop.Excel.Worksheet
+
+        Try
+            'Añadimos el Libro al programa, y la hoja al libro
+            exLibro = exApp.Workbooks.Add
+            exHoja = exLibro.Worksheets.Add()
+
+            ' ¿Cuantas columnas y cuantas filas?
+            Dim NCol As Integer = ElGrid.ColumnCount
+            Dim NRow As Integer = ElGrid.RowCount
+
+            'Aqui recorremos todas las filas, y por cada fila todas las columnas
+            'y vamos escribiendo.
+            Dim cont As Integer = 1
+            For i As Integer = 0 To NCol - 1
+                If (ElGrid.Columns(i).Visible = True) Then
+                    exHoja.Cells.Item(1, cont) = ElGrid.Columns(i).HeaderText
+
+                    cont += 1
+                End If
+            Next
+
+
+            For Fila As Integer = 0 To NRow - 1
+                Dim c As Integer = 0
+                For Col As Integer = 0 To NCol - 1
+                    If (ElGrid.Rows(0).Cells(Col).Visible) Then
+                        exHoja.Cells.Item(Fila + 2, c + 1) = ElGrid.Item(Col, Fila).Value
+                        c += 1
+                    End If
+                Next
+            Next
+            'Titulo en negrita, Alineado al centro y que el tamaño de la columna
+            'se ajuste al texto
+            exHoja.Rows.Item(1).Font.Bold = 1
+            exHoja.Rows.Item(1).HorizontalAlignment = 3
+            exHoja.Columns.AutoFit()
+            'Aplicación visible
+            exApp.Application.Visible = True
+            exHoja = Nothing
+            exLibro = Nothing
+            exApp = Nothing
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al exportar a Excel")
+            Return False
+        End Try
+        Return True
+    End Function
+
+    Sub importarTxt()
+        If conexionsql.State <> ConnectionState.Open Then
+            conexionsql.Open()
+        End If
+        Dim cad As String = Nothing
+        Dim DocumentoDialog As New OpenFileDialog()
+
+        With DocumentoDialog
+            .InitialDirectory = "D:\ProyectoHope"
+            .Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+            .FilterIndex = 2
+            .RestoreDirectory = True
+            .Title = "Abrir Archivo"
+            .ShowDialog()
+        End With
+        If DocumentoDialog.FileName.ToString <> "" Then
+            Try
+                'conexionsql.ConnectionString = "Data Source ='.'; Initial Catalog = 'INVENTARIO_DB'; Integrated security = true "
+                'conexionsql.Open()
+                cad = DocumentoDialog.FileName
+                If (cad IsNot Nothing) Then
+                    comando.CommandText = "BULK INSERT PRODUCTO FROM '" & cad & "' WITH ( FIELDTERMINATOR= ',', ROWTERMINATOR = '\n' );"
+                    comando.ExecuteNonQuery()
+                    MessageBox.Show("SE INSERTÓ EL REGISTRO DE FORMA CORRECTA", "ÉXITO")
+                    Producto.ShowDialog()
+
+                Else
+                    MessageBox.Show("NO SE SELECCIONO ALGUN ARCHIVO", "ATENCIÓN")
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information, "Informacion")
+            Finally
+                conexionsql.Close()
+            End Try
+        End If
+        ' MsgBox("Se ha cargado la importacion correctamente", MsgBoxStyle.Information, "Importado con exito")
+    End Sub
+End Module
